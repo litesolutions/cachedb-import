beb413135c015b7e8b95cd2ff4a18ff9f42018b4
Francis Galiegue <fgaliegue@gmail.com>
2016-02-12 19:26:02 +0100

    CacheRunner: try an alternate implementation with Load instead of LoadStream
    But it doesn't work at all.
    
    The file is created for this but for some reason Caché is unable to read it
    back.

--- src/main/java/es/litesolutions/cache/CacheRunner.java
+++ src/main/java/es/litesolutions/cache/CacheRunner.java
@@ -4,6 +4,7 @@ import com.github.fge.lambdas.functions.ThrowingFunction;
 import com.intersys.cache.Dataholder;
 import com.intersys.classes.CharacterStream;
 import com.intersys.classes.Dictionary.ClassDefinition;
+import com.intersys.classes.FileCharacterStream;
 import com.intersys.classes.GlobalCharacterStream;
 import com.intersys.objects.CacheException;
 import com.intersys.objects.Database;
@@ -30,8 +31,11 @@ public final class CacheRunner
 {
     private static final String CLASSDEFINITION_NAME_SQLFIELD = "Name";
 
-    private static final String LOAD_CLASSNAME = "%SYSTEM.OBJ";
-    private static final String LOAD_METHODNAME = "LoadStream";
+    private static final String LOADSTREAM_CLASSNAME = "%SYSTEM.OBJ";
+    private static final String LOADSTREAM_METHODNAME = "LoadStream";
+
+    private static final String LOADFILE_CLASSNAME = "%SYSTEM.OBJ";
+    private static final String LOADFILE_METHODNAME = "Load";
 
     private final CacheDb cacheDb;
 
@@ -99,7 +103,7 @@ public final class CacheRunner
         arguments[2] = Dataholder.create(errorlog.value);
         // arg 4: loadedlist
         arguments[3] = Dataholder.create(loadedlist.value);
-        // arg 5: listonly; we want true
+        // arg 5: listonly; no
         arguments[4] = Dataholder.create(Boolean.FALSE);
         // arg 6: selecteditems; nothing
         arguments[5] = Dataholder.create(null);
@@ -110,8 +114,8 @@ public final class CacheRunner
 
         // Now, make the call
         final Dataholder[] result = db.runClassMethod(
-            LOAD_CLASSNAME,
-            LOAD_METHODNAME,
+            LOADSTREAM_CLASSNAME,
+            LOADSTREAM_METHODNAME,
             byRefArgs,
             arguments,
             Database.RET_PRIM
@@ -136,6 +140,95 @@ public final class CacheRunner
 //
 //        loadedlist.set(result[2].getString());
 //        System.out.println("loadedlist: " + loadedlist.getValue());
+    }
+
+    public void importXml2(final Path path)
+        throws CacheException, IOException
+    {
+        final Database db = cacheDb.getDatabase();
+
+        final FileCharacterStream stream = new FileCharacterStream(db);
+
+
+        loadContent(stream, path);
+
+        final String remoteFileName = stream._filenameGet();
+
+        /*
+         * Arguments for class "%SYSTEM.OBJ", class method "Load"
+         */
+        final Dataholder[] arguments = new Dataholder[9];
+
+        /*
+         * Arguments ByRef
+         *
+         * Indices start at 1, not 0
+         */
+        final int[] byRefArgs = new int[3];
+
+        // Arg 3: error log
+        final StringHolder errorlog = new StringHolder("");
+        byRefArgs[0] = 3;
+
+        // Arg 4: list of loaded items
+        final StringHolder loadedlist = new StringHolder("");
+        byRefArgs[1] = 4;
+
+        // Arg 9: description (?)
+        final StringHolder description = new StringHolder("");
+        byRefArgs[2] = 9;
+
+        /*
+         * Fill arguments
+         */
+        // arg 1: file name
+        arguments[0] = Dataholder.create(remoteFileName);
+        // arg 2: qspec; we want to ensure that compile works, at least
+        arguments[1] = new Dataholder("c");
+        // arg 3: errorlog
+        arguments[2] = Dataholder.create(errorlog.value);
+        // arg 4: loadedlist
+        arguments[3] = Dataholder.create(loadedlist.value);
+        // arg 5: listonly; no
+        arguments[4] = Dataholder.create(Boolean.FALSE);
+        // arg 6: selecteditems; nothing
+        arguments[5] = Dataholder.create(null);
+        // arg 7: displayname. For logging...
+        arguments[6] = Dataholder.create(null);
+        // arg 8: charset. Default is empty string, we'll assume UTF-8.
+        arguments[7] = new Dataholder((String) null);
+        // arg 9: description (?)
+        arguments[8] = Dataholder.create(description.value);
+
+        // Now, make the call
+        final Dataholder[] result = db.runClassMethod(
+            LOADFILE_CLASSNAME,
+            LOADFILE_METHODNAME,
+            byRefArgs,
+            arguments,
+            Database.RET_PRIM
+        );
+
+        /*
+         * The result normally has three members:
+         *
+         * - first is the status; and we need to do that:
+         */
+        db.parseStatus(result[0]);
+
+        /*
+         * - others are ByRef arguments
+         */
+        // FIXME: not filled correctly... No idea if this is a bug with
+        // InterSystems jars or not. See the README for more details.
+        // Ideally this should return a Set<String> containing the loaded
+        // classes names...
+//            errorlog.set(result[1].getString());
+//            System.out.println("errorlog: " + errorlog.getValue());
+//
+//            loadedlist.set(result[2].getString());
+//            System.out.println("loadedlist: " + loadedlist.getValue());
+
     }
 
     private static void loadContent(final CharacterStream stream,
