cefeb8cbfa905398afe7eedcc7f87c6e7ba3bddc
Francis Galiegue <fgaliegue@gmail.com>
2016-02-14 08:03:13 +0100

    CacheRunner: rename some methods; remove binary import method

--- src/main/java/es/litesolutions/cache/CacheRunner.java
+++ src/main/java/es/litesolutions/cache/CacheRunner.java
@@ -1,10 +1,8 @@
 package es.litesolutions.cache;
 
 import com.intersys.cache.Dataholder;
-import com.intersys.classes.BinaryStream;
 import com.intersys.classes.CharacterStream;
 import com.intersys.classes.Dictionary.ClassDefinition;
-import com.intersys.classes.FileBinaryStream;
 import com.intersys.classes.FileCharacterStream;
 import com.intersys.classes.GlobalCharacterStream;
 import com.intersys.objects.CacheException;
@@ -78,7 +76,7 @@ public final class CacheRunner
         return Collections.unmodifiableSet(set);
     }
 
-    public void importXml(final Path path)
+    public void importStream(final Path path)
         throws CacheException, IOException
     {
         final Database db = cacheDb.getDatabase();
@@ -149,8 +147,6 @@ public final class CacheRunner
          */
         // FIXME: not filled correctly... No idea if this is a bug with
         // InterSystems jars or not. See the README for more details.
-        // Ideally this should return a Set<String> containing the loaded
-        // classes names...
 //        errorlog.set(result[1].getString());
 //        System.out.println("errorlog: " + errorlog.getValue());
 //
@@ -158,8 +154,7 @@ public final class CacheRunner
 //        System.out.println("loadedlist: " + loadedlist.getValue());
     }
 
-    // DOESN'T WORK :(
-    public void importXml2(final Path path)
+    public void importFile(final Path path)
         throws CacheException, IOException
     {
         final String tempFileName = createRemoteTemporaryFileName();
@@ -279,92 +274,6 @@ public final class CacheRunner
         }
     }
 
-    public void importXml3(final Path path)
-        throws CacheException, IOException
-    {
-        final String tempFileName = createRemoteTemporaryFileName();
-
-        final Database db = cacheDb.getDatabase();
-
-        final FileBinaryStream stream = new FileBinaryStream(db);
-        stream._filenameSet(tempFileName);
-
-        loadBinary(stream, path);
-
-        final String remoteFileName = stream._filenameGet();
-
-        /*
-         * Arguments for class "%SYSTEM.OBJ", class method "Load"
-         */
-        final Dataholder[] arguments = new Dataholder[9];
-
-        /*
-         * Arguments ByRef
-         *
-         * Indices start at 1, not 0
-         */
-        final int[] byRefArgs = new int[3];
-
-        // Arg 3: error log
-        final StringHolder errorlog = new StringHolder("");
-        byRefArgs[0] = 3;
-
-        // Arg 4: list of loaded items
-        final StringHolder loadedlist = new StringHolder("");
-        byRefArgs[1] = 4;
-
-        // Arg 9: description (?)
-        final StringHolder description = new StringHolder("");
-        byRefArgs[2] = 9;
-
-        /*
-         * Fill arguments
-         */
-        // arg 1: file name
-        arguments[0] = Dataholder.create(remoteFileName);
-        // arg 2: qspec; we want to ensure that compile works, at least
-        arguments[1] = new Dataholder("d");
-        // arg 3: errorlog
-        arguments[2] = Dataholder.create(errorlog.value);
-        // arg 4: loadedlist
-        arguments[3] = Dataholder.create(loadedlist.value);
-        // arg 5: listonly; no
-        arguments[4] = Dataholder.create(Boolean.FALSE);
-        // arg 6: selecteditems; nothing
-        arguments[5] = Dataholder.create(null);
-        // arg 7: displayname. For logging...
-        arguments[6] = Dataholder.create(null);
-        // arg 8: charset. Default is empty string, we'll assume UTF-8.
-        arguments[7] = new Dataholder("UTF8");
-        // arg 9: description (?)
-        arguments[8] = Dataholder.create(description.value);
-
-        // Now, make the call
-        final Dataholder[] result = db.runClassMethod(
-            LOADFILE_CLASSNAME,
-            LOADFILE_METHODNAME,
-            byRefArgs,
-            arguments,
-            Database.RET_PRIM
-        );
-
-        /*
-         * The result normally has three members:
-         *
-         * - first is the status; and we need to do that:
-         */
-        db.parseStatus(result[0]);
-
-        /*
-         * - others are ByRef arguments
-         */
-        errorlog.set(result[1].getString());
-        System.out.println("errorlog: " + errorlog.getValue());
-
-        loadedlist.set(result[2].getString());
-        System.out.println("loadedlist: " + loadedlist.getValue());
-    }
-
     private String createRemoteTemporaryFileName()
         throws CacheException
     {
@@ -393,12 +302,5 @@ public final class CacheRunner
         // Note that we don't _rewind() the stream; the loading function does
         // that by itself
         stream._write(sb.toString());
-    }
-
-    private static void loadBinary(final BinaryStream stream,
-        final Path path)
-        throws IOException, CacheException
-    {
-        Files.copy(path, stream.getOutputStream());
     }
 }
